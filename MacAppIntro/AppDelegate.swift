ebcce4ef43f2d78a9dd2cb1c1f6b40f31fb19165
samuraipapa <yury.gitman@gmail.com>
2015-04-06 10:49:37 -0400

    Initial Commit

---
+++ MacAppIntro/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacAppIntro
+//
+//  Created by GrownYoda on 4/6/15.
+//  Copyright (c) 2015 yuryg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MacAppIntro/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MacAppIntro
+//
+//  Created by GrownYoda on 4/6/15.
+//  Copyright (c) 2015 yuryg. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
